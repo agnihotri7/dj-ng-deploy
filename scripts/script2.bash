ab2bf113e91d24c4555561379a317c648fe28a51
pradeep <pradeep.k@finoit.co.in>
2017-12-06 14:51:40 +0530

    add deployment scripts

---
+++ scripts/script2.bash
@@ -0,0 +1,23 @@
+#!/bin/bash
+echo "Reading configurations ..."
+source ../config/server.conf
+
+PROJ_ROOT="/home/$USERNAME/$PROJECT_NAME.com"
+PROJ_DIR="/home/$USERNAME/$PROJECT_NAME.com/$USERNAME"
+
+cd $PROJ_DIR
+echo "creating virtualenv with python 3.5 inside env dir and installing requirements..."
+virtualenv -p /usr/bin/python3.5 env
+source env/bin/activate
+pip install -r requirements.txt
+pip install gunicorn
+
+echo "applying migrations ..."
+python manage.py migrate
+
+echo "creating super user ..."
+python manage.py createsuperuser
+
+echo "using collectstatic ..."
+python manage.py collectstatic
+echo "Now switch to root and run script3 ..."
